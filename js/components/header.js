5f22ee20d83405715dc93c24d68f893f936f7d57
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-20 19:19:54 +0530

    Update header.js

--- js/components/header.js
+++ js/components/header.js
@@ -1,1 +1,31 @@
-// HEADER COMPONENT
+export function renderHeader() {
+
+  return `
+  
+    <header class="header">
+
+      <div class="header-left">
+
+        <img
+          src="assets/myntra-pricing.jpg"
+          class="logo"
+        >
+
+        <div>
+
+          <div class="app-title">
+            Myntra Reverse Pricing
+          </div>
+
+          <div class="app-subtitle">
+            Reverse Settlement & Pricing Engine
+          </div>
+
+        </div>
+
+      </div>
+
+    </header>
+
+  `;
+}
