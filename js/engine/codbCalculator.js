c9e0dcb4cc3012855e4a35c9cd1791ccb33b1402
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-20 19:14:41 +0530

    Update codbCalculator.js

--- js/engine/codbCalculator.js
+++ js/engine/codbCalculator.js
@@ -1,1 +1,38 @@
-// CODB CALCULATOR
+import {
+  BUSINESS_RULES
+} from '../config/businessRules.js';
+
+export function calculateCODB({
+  fixedFee,
+  returnFee
+}) {
+
+  const baseReturnCost =
+    Number(fixedFee) +
+    Number(returnFee);
+
+  const returnCost =
+    baseReturnCost *
+    BUSINESS_RULES.GST_MULTIPLIER;
+
+  const rtvPercent =
+    BUSINESS_RULES.RTV_PERCENT;
+
+  const rtvCodb =
+    (
+      returnCost * rtvPercent
+    ) /
+    (
+      100 - rtvPercent
+    );
+
+  return {
+    baseReturnCost,
+
+    returnCost,
+
+    rtvPercent,
+
+    rtvCodb
+  };
+}
