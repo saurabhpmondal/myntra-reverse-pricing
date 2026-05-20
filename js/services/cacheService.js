cbc21e64b8b2721825913d7ba663d38d11c85fa9
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-20 18:50:27 +0530

    Update cacheService.js

--- js/services/cacheService.js
+++ js/services/cacheService.js
@@ -1,1 +1,23 @@
-// CACHE SERVICE
+export const appCache = {
+  productMaster: [],
+  commercialsMaster: [],
+  gtaMaster: [],
+
+  productMap: {},
+  commercialMap: {},
+  gtaMap: {},
+
+  initialized: false
+};
+
+export function resetCache() {
+  appCache.productMaster = [];
+  appCache.commercialsMaster = [];
+  appCache.gtaMaster = [];
+
+  appCache.productMap = {};
+  appCache.commercialMap = {};
+  appCache.gtaMap = {};
+
+  appCache.initialized = false;
+}
