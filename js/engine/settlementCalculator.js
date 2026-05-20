b27d4da2924bd870f049d9be15419c29d1253063
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-20 19:15:02 +0530

    Update settlementCalculator.js

--- js/engine/settlementCalculator.js
+++ js/engine/settlementCalculator.js
@@ -1,1 +1,419 @@
-// SETTLEMENT CALCULATOR
+import {
+  BUSINESS_RULES
+} from '../config/businessRules.js';
+
+import {
+  calculateGTA
+} from './gtaCalculator.js';
+
+import {
+  calculateCommercials
+} from './commercialCalculator.js';
+
+import {
+  calculateCODB
+} from './codbCalculator.js';
+
+function round2(value) {
+  return Number(
+    Number(value || 0).toFixed(2)
+  );
+}
+
+function ceil(value) {
+  return Math.ceil(
+    Number(value || 0)
+  );
+}
+
+export function calculateSettlement({
+  brand,
+  articleType,
+  sellingPrice,
+  tp = 0,
+  mrp = 0
+}) {
+
+  const SP =
+    Number(sellingPrice);
+
+  const TP =
+    Number(tp);
+
+  const MRP =
+    Number(mrp);
+
+  /*
+  -----------------------------------
+  GTA
+  -----------------------------------
+  */
+
+  const gtaData =
+    calculateGTA({
+      brand,
+      articleType,
+      sellingPrice: SP
+    });
+
+  const gtaCharge =
+    round2(gtaData.gtaCharge);
+
+  /*
+  -----------------------------------
+  SELLER PRICE
+  -----------------------------------
+  */
+
+  const sellerPrice =
+    round2(
+      SP - gtaCharge
+    );
+
+  /*
+  -----------------------------------
+  COMMERCIALS
+  -----------------------------------
+  */
+
+  const commercialData =
+    calculateCommercials({
+      brand,
+      articleType,
+      sellerPrice
+    });
+
+  const commissionPercent =
+    commercialData.commissionPercent;
+
+  const commissionRs =
+    round2(
+      sellerPrice *
+      (
+        commissionPercent / 100
+      )
+    );
+
+  const fixedFee =
+    round2(
+      commercialData.fixedFee
+    );
+
+  /*
+  -----------------------------------
+  GST ON COM + FIXED FEE
+  -----------------------------------
+  */
+
+  const gstOnComAndFee =
+    ceil(
+      (
+        commissionRs +
+        fixedFee
+      ) *
+      (
+        BUSINESS_RULES.GST_PERCENT / 100
+      )
+    );
+
+  /*
+  -----------------------------------
+  UPLOAD SETTLEMENT
+  -----------------------------------
+  */
+
+  const uploadSettlement =
+    round2(
+      sellerPrice -
+      commissionRs -
+      fixedFee -
+      gstOnComAndFee
+    );
+
+  /*
+  -----------------------------------
+  TDS + TCS
+  -----------------------------------
+  */
+
+  const tds =
+    round2(
+      sellerPrice *
+      (
+        BUSINESS_RULES.TDS_PERCENT / 100
+      )
+    );
+
+  const tcs =
+    round2(
+      sellerPrice *
+      (
+        BUSINESS_RULES.TCS_PERCENT / 100
+      )
+    );
+
+  const totalTaxDeduction =
+    round2(
+      tds + tcs
+    );
+
+  /*
+  -----------------------------------
+  BANK SETTLEMENT
+  -----------------------------------
+  */
+
+  const bankSettlement =
+    round2(
+      uploadSettlement -
+      totalTaxDeduction
+    );
+
+  /*
+  -----------------------------------
+  ROYALTY + MARKETING
+  -----------------------------------
+  */
+
+  const royaltyPercent =
+    commercialData.royaltyPercent;
+
+  const marketingPercent =
+    commercialData.marketingPercent;
+
+  const royalty =
+    round2(
+      SP *
+      (
+        royaltyPercent / 100
+      )
+    );
+
+  const marketing =
+    round2(
+      SP *
+      (
+        marketingPercent / 100
+      )
+    );
+
+  const gstOnRoyaltyMarketing =
+    round2(
+      (
+        royalty +
+        marketing
+      ) *
+      (
+        BUSINESS_RULES.GST_PERCENT / 100
+      )
+    );
+
+  /*
+  -----------------------------------
+  PAYOUT BEFORE CODB
+  -----------------------------------
+  */
+
+  const payoutBeforeCODB =
+    round2(
+      bankSettlement -
+      royalty -
+      marketing -
+      gstOnRoyaltyMarketing
+    );
+
+  /*
+  -----------------------------------
+  DISPATCH COST
+  -----------------------------------
+  */
+
+  const dispatchCost =
+    SP < 1000
+      ? BUSINESS_RULES.DISPATCH_BELOW_1000
+      : BUSINESS_RULES.DISPATCH_ABOVE_1000;
+
+  /*
+  -----------------------------------
+  CODB
+  -----------------------------------
+  */
+
+  const codbData =
+    calculateCODB({
+      fixedFee,
+      returnFee:
+        commercialData.returnFee
+    });
+
+  /*
+  -----------------------------------
+  FINAL PAYOUT
+  -----------------------------------
+  */
+
+  const payoutAfterCODB =
+    round2(
+      payoutBeforeCODB -
+      dispatchCost -
+      codbData.rtvCodb
+    );
+
+  /*
+  -----------------------------------
+  TP PROFIT
+  -----------------------------------
+  */
+
+  const tpProfitRs =
+    round2(
+      payoutAfterCODB - TP
+    );
+
+  const tpProfitPercent =
+    TP > 0
+      ? round2(
+          (
+            tpProfitRs / TP
+          ) * 100
+        )
+      : 0;
+
+  /*
+  -----------------------------------
+  TD %
+  -----------------------------------
+  */
+
+  const tradeDiscount =
+    MRP > 0
+      ? round2(
+          (
+            (
+              MRP - SP
+            ) / MRP
+          ) * 100
+        )
+      : 0;
+
+  return {
+
+    /*
+    INPUTS
+    */
+
+    brand,
+    articleType,
+
+    mrp: MRP,
+    tp: TP,
+
+    sellingPrice: SP,
+
+    /*
+    GTA
+    */
+
+    gtaCharge,
+
+    /*
+    SELLER PRICE
+    */
+
+    sellerPrice,
+
+    /*
+    COMMERCIALS
+    */
+
+    commissionPercent,
+    commissionRs,
+
+    fixedFee,
+
+    /*
+    GST
+    */
+
+    gstOnComAndFee,
+
+    /*
+    UPLOAD
+    */
+
+    uploadSettlement,
+
+    /*
+    TAXES
+    */
+
+    tds,
+    tcs,
+
+    totalTaxDeduction,
+
+    /*
+    BANK
+    */
+
+    bankSettlement,
+
+    /*
+    ROYALTY
+    */
+
+    royaltyPercent,
+    royalty,
+
+    marketingPercent,
+    marketing,
+
+    gstOnRoyaltyMarketing,
+
+    /*
+    PAYOUT
+    */
+
+    payoutBeforeCODB,
+
+    /*
+    DISPATCH
+    */
+
+    dispatchCost,
+
+    /*
+    CODB
+    */
+
+    baseReturnCost:
+      round2(
+        codbData.baseReturnCost
+      ),
+
+    returnCost:
+      round2(
+        codbData.returnCost
+      ),
+
+    rtvPercent:
+      codbData.rtvPercent,
+
+    rtvCodb:
+      round2(
+        codbData.rtvCodb
+      ),
+
+    /*
+    FINAL
+    */
+
+    payoutAfterCODB,
+
+    tpProfitRs,
+
+    tpProfitPercent,
+
+    tradeDiscount
+  };
+}
