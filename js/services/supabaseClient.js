11fa8b7dbbb9afd28e8f508ae178b0d19b3b97ad
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-20 18:50:09 +0530

    Update supabaseClient.js

--- js/services/supabaseClient.js
+++ js/services/supabaseClient.js
@@ -1,1 +1,8 @@
-// SUPABASE CLIENT PLACEHOLDER
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+import { SUPABASE_CONFIG } from '../config/supabaseConfig.js';
+
+export const supabase = createClient(
+  SUPABASE_CONFIG.URL,
+  SUPABASE_CONFIG.ANON_KEY
+);
