ed2dacabe152ca09cb32ed5a000b1c3fc93546d1
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-22 16:28:01 +0530

    Create supabaseService.js

---
+++ js/services/supabaseService.js
@@ -0,0 +1,12 @@
+import {
+  createClient
+} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+export const supabase =
+  createClient(
+
+    'https://mkaypthahezxsjldbvnf.supabase.co',
+
+    'sb_publishable_pSu0F0Dx4J6JPyNkdBCRhw_3Rr1Jxwo'
+
+  );
